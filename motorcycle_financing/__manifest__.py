b93b9f3947f54ffb42fe33ddfdba9dd378a1bf3f
juanchomang <juan604@gmail.com>
2025-06-10 11:19:19 -0700

    2.01 Module Introduction - Defining the Manifest file

---
+++ motorcycle_financing/__manifest__.py
@@ -0,0 +1,18 @@
+# Manifest Template. If you copy this template, make sure the indentation is right.
+{
+    'name': 'Motorcycle Financing',
+    'summary': 'Streamlines the loan application process for dealerships.',
+    'license': 'OPL-1',
+    'category': 'Kawiil/Custom Modules',
+    'author': 'Juan Herrera de la Fuente',
+    'website': 'https://github.com/juanchomang/kawiil_base',
+    'version': '18.0.1.0.0',
+    'depends': ['base'],
+    'data': [
+
+    ],
+    'demo': [
+
+    ],
+    'application': True,
+}
